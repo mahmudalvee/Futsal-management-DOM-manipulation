d3527d3f3a49c25167dafea0ccbe522264d72965
mahmudalvee <mahmud2.alvee@gmail.com>
2022-08-22 21:33:48 +0600

    prothom hishab

--- index.js
+++ index.js
@@ -41,3 +41,48 @@ function addPlayers(element)
 }
 
 
+//prothom hishab
+function getInputValueById(inputFieldId){
+    // GET any inputfield value
+    const inputField= document.getElementById(inputFieldId);
+    const inputFieldString= inputField.value;
+    const inputFieldValue= parseFloat(inputFieldString);
+    inputField.value= '';
+    return inputFieldValue;
+}
+
+function getElementValueTextById(elementId){
+    // GET any innerText value from html
+    const element= document.getElementById(elementId);
+    const elementValueTextString= element.innerText;
+    const elementValueText= parseFloat(elementValueTextString);
+    return elementValueText;
+}
+
+function prothomHishab(a,b){
+    return ans= a*b;
+}
+
+function finalHishab(a,b,c){
+    return ans= a+b+c;
+}
+
+document.getElementById('calulate-btn').addEventListener('click',function(){
+    // get player cost
+    const perPlayerAmount= getInputValueById('per-player-input');
+    if(isNaN(perPlayerAmount) || perPlayerAmount < 0)
+    {
+        alert('Calculation Error!!!\nPlease enter valid digit cost$ of Players.');
+        return;
+    }
+    // total hishab
+    if(selectedPlayers.length>5){
+        return;
+    }
+    //pass parameters to function prothomHishab()
+    const playerExpensesTotalAmount= prothomHishab(perPlayerAmount, selectedPlayers.length);
+    
+    // setting the total cost where it belongs (html er id, value)
+    setElementValueTextById('playerExpensesAmount',playerExpensesTotalAmount);
+    return playerExpensesTotalAmount;
+})
